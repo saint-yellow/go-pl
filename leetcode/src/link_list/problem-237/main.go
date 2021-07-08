7846987fadd858a428d241f3fd2b0f54ed756a8d
黄炯辉 <saint-yellow@hotmail.com>
2021-07-08 22:07:38 +0800

    commit solution(s) (with comments) for problems

--- leetcode/src/link_list/problem-237/main.go
+++ leetcode/src/link_list/problem-237/main.go
@@ -1,11 +1,42 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Value int
 	Next *ListNode
 }
 
+func (node *ListNode) toList() []int {
+	result := make([]int, 0)
+	for node != nil {
+		result = append(result, node.Value)
+		node = node.Next
+	}
+	return result
+}
+
 func deleteNode(node *ListNode) {
+	method1(node)
+}
+
+func method1(node *ListNode) {
 	node.Value = node.Next.Value
 	node.Next = node.Next.Next
+}
+
+func main() {
+	list := &ListNode{
+		Value: 1,
+		Next: &ListNode{
+			Value: 2,
+			Next: &ListNode{
+				Value: 3,
+				Next: nil,
+			},
+		},
+	}
+	fmt.Println(list.toList())
+	deleteNode(list)
+	fmt.Println(list.toList())
 }
