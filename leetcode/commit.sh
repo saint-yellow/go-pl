9faa5eb336e3c8f9e7515052b9247c5e89c4d5ef
黄炯辉 <saint-yellow@hotmail.com>
2021-07-29 12:39:40 +0800

    update commit.sh

--- leetcode/commit.sh
+++ leetcode/commit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-git commit -am "commit solution(s) (with comments) for problems"
+git commit -am "commit solution(s) (with comments) for problem(s)"
 git push -u origin master
