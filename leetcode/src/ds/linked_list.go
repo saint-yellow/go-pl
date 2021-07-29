ad139c3536d739ebf467daf3cd3a719dcbce14c2
黄炯辉 <saint-yellow@hotmail.com>
2021-07-29 12:40:23 +0800

    modify APIs of data structures

--- leetcode/src/ds/linked_list.go
+++ leetcode/src/ds/linked_list.go
@@ -26,6 +26,15 @@ func BuildSinglyLinkedList(values []int) *SinglyLinkedNode {
 	return sentinel.Next
 }
 
+func (node *SinglyLinkedNode) Length() int {
+	count := 0
+	for node != nil {
+		count += 1
+		node = node.Next
+	}
+	return count
+}
+
 
 type DoublyLinkedNode struct {
 	Val int
@@ -43,4 +52,22 @@ func BuildDoublyLinkedList(values []int) *DoublyLinkedNode {
 	}
 	
 	return sentinel.Next
+}
+
+func (node *DoublyLinkedNode) ToList(reverse bool) []int {
+	result := make([]int, 0)
+
+	if reverse {
+		for node != nil && node.Previous != nil {
+			result = append(result, node.Val)
+			node = node.Previous
+		}
+		return result
+	}
+
+	for node != nil && node.Next != nil {
+		result = append(result, node.Val)
+		node = node.Next
+	}
+	return result
 }
