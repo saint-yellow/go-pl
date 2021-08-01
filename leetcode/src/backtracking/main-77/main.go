8c64b4dec1b11979879d18d6e4fa2d6e0b4b90a3
黄炯辉 <saint-yellow@hotmail.com>
2021-08-01 22:05:43 +0800

    commit solution(s) (with comments) for problem(s)

--- leetcode/src/backtracking/main-77/main.go
+++ leetcode/src/backtracking/main-77/main.go
@@ -4,39 +4,39 @@ package main
 
 import "fmt"
 
-var (
-	result [][]int
-	path []int
-)
-
-func backtracking(n, k, startIndex int) {
-	if len(path) == k {
-		temp := make([]int, 2)
-		copy(temp, path)
-		result = append(result, temp)
-		return
-	}
-
-	for i := startIndex; i <= n; i++ {
-		path = append(path, i)
-		backtracking(n, k, i + 1)
-		path = path[0:len(path) - 1]
-	}
-}
-
 func combine(n, k int) [][]int {
 	return method1(n, k)
 }
 
+// 基于回溯的解法
 func method1(n, k int) [][]int {
+	result := make([][]int, 0) 
+	path := make([]int, 0)
 	if n <= 0 || k <= 0 || k > n {
 		return result
 	}
+
+	var backtracking func(n, k, startIndex int)
+	backtracking = func(n, k, startIndex int) {
+		if len(path) == k {
+			temp := make([]int, k)
+			copy(temp, path)
+			result = append(result, temp)
+			return
+		}
+
+		for i := startIndex; i <= n - (k - len(path)) + 1; i++ {
+			path = append(path, i)
+			backtracking(n, k, i + 1)
+			path = path[0:len(path) - 1]
+		}
+	}
+
 	backtracking(n, k, 1)
 	return result
 }
 
 func main() {
-	combine(1, 1)
-	fmt.Println(result)
+	c := combine(4, 3)
+	fmt.Println(c)
 }
