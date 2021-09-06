a2fccc80c3500706f2af20c9156cad9ddffb7e44
黄炯辉 <saint-yellow@hotmail.com>
2021-09-06 11:50:18 +0800

    commit solution(s) (with comments) for problem(s)

--- leetcode/src/problems/array/main-15/main.go
+++ leetcode/src/problems/array/main-15/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 )
 
 func threeSum(numbers []int) [][]int {
+	return method1(numbers)
+}
+
+
+func method1(numbers []int) [][]int {
 	result := make([][]int, 0)
 	if numbers == nil || len(numbers) < 3 {
 		return result
@@ -42,4 +48,48 @@ func threeSum(numbers []int) [][]int {
 	}
 
 	return result
+}
+
+func method2(numbers []int) [][]int {
+	sort.Ints(numbers)
+
+	length := len(numbers)
+	result := [][]int{}
+	for i := 0; i < length-2; i++ {
+		n1 := numbers[i]
+		if n1 > 0 {
+			break
+		}
+
+		if i > 0 && n1 == numbers[i-1] {
+			continue
+		}
+
+		left, right := i+1, length-1
+		for left < right {
+			n2, n3 := numbers[left], numbers[right]
+
+			if n1 + n2 + n3 == 0 {
+				result = append(result, []int{n1, n2, n3})
+
+				for left < right && numbers[left] == n2 {
+					left++
+				}
+
+				for left < right && numbers[right] == n3 {
+					right--
+				}
+			} else if n1 + n2 + n3 > 0 {
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, 4}))
+	fmt.Println(method2([]int{-1, 0, 1, 2, -1, 4}))
 }
