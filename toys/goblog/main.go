c645ac0c1c8791f89e9231d28b4d9d42ac30d3a0
黄炯辉 <saint-yellow@hotmail.com>
2022-01-20 14:32:21 +0800

    重构  initDB 和 createTables

--- toys/goblog/main.go
+++ toys/goblog/main.go
@@ -12,6 +12,7 @@ import (
 	"unicode/utf8"
 
 	"github.com/gorilla/mux"
+	"github.com/saint-yellow/go-pl/toys/goblog/pkg/database"
 	"github.com/saint-yellow/go-pl/toys/goblog/pkg/logger"
 	"github.com/saint-yellow/go-pl/toys/goblog/pkg/route"
 	"github.com/saint-yellow/go-pl/toys/goblog/pkg/types"
@@ -19,8 +20,10 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
-var router *mux.Router
-var db *sql.DB
+var ( 
+    router *mux.Router
+    db *sql.DB
+)
 
 func initDB() {
 
@@ -494,8 +497,8 @@ func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    initDB()
-    createTables()
+    database.Initialize()
+    db = database.DB
 
     route.Initialize()
     router = route.Router
