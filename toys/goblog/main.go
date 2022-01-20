f5c6b47a9dc75fd9068666d1772d05fd5d5b03fd
黄炯辉 <saint-yellow@hotmail.com>
2022-01-20 12:52:28 +0800

    显示文章

--- toys/goblog/main.go
+++ toys/goblog/main.go
@@ -70,10 +70,34 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
 
+type Article struct {
+    Title, Body string
+    ID int64
+}
+
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     id := vars["id"]
-    fmt.Fprint(w, "文章 ID："+id)
+    
+    article := Article{}
+    query := "select * from articles where id = ?"
+    err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
+
+    if err != nil {
+        if err == sql.ErrNoRows {
+            w.WriteHeader(http.StatusNotFound)
+            fmt.Fprint(w, "404 文章未找到")
+        } else {
+            checkError(err)
+            w.WriteHeader(http.StatusInternalServerError)
+            fmt.Fprint(w, "500 服务器内部错误")
+        }
+    } else {
+        tmpl, err := template.ParseFiles("resources/views/articles/show.gohtml")
+        checkError(err)
+        err = tmpl.Execute(w, article)
+        checkError(err)
+    }
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
