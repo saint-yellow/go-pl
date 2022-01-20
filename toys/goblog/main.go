281fa80cdac5265956dfcf7e7e704cfe4dfc3032
黄炯辉 <saint-yellow@hotmail.com>
2022-01-20 23:45:46 +0800

    重构中间件

--- toys/goblog/main.go
+++ toys/goblog/main.go
@@ -2,45 +2,17 @@ package main
 
 import (
 	"net/http"
-	"strings"
 
-	"github.com/gorilla/mux"
+	"github.com/saint-yellow/go-pl/toys/goblog/app/http/middlewares"
 	"github.com/saint-yellow/go-pl/toys/goblog/bootstrap"
-	"github.com/saint-yellow/go-pl/toys/goblog/pkg/database"
+	"github.com/saint-yellow/go-pl/toys/goblog/pkg/logger"
 )
-
-var ( 
-    router *mux.Router
-)
-
-// 充当中间件
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        // 1. 设置标头
-        w.Header().Set("Content-Type", "text/html; charset=utf-8")
-        // 2. 继续处理请求
-        next.ServeHTTP(w, r)
-    })
-}
-
-func removeTailingSlash(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if r.URL.Path != "/" {
-            r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-        }
-        next.ServeHTTP(w, r)
-    })
-}
 
 func main() {
-    database.Initialize()
 
     bootstrap.SetupDB()
+    router := bootstrap.SetupRoute()
 
-    router = bootstrap.SetupRoute()
-
-    // 中间件：强制内容类型为 HTML
-    router.Use(forceHTMLMiddleware)
-
-    http.ListenAndServe(":3000", removeTailingSlash(router))
+    err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+    logger.LogError(err)
 }
