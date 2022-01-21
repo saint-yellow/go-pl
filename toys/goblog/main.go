6c09255116b3e4626d4ab0fc7ff1bf648c69562e
黄炯辉 <saint-yellow@hotmail.com>
2022-01-22 03:14:01 +0800

    配置和环境变量

--- toys/goblog/main.go
+++ toys/goblog/main.go
@@ -5,12 +5,20 @@ import (
 
 	"github.com/saint-yellow/go-pl/toys/goblog/app/http/middlewares"
 	"github.com/saint-yellow/go-pl/toys/goblog/bootstrap"
+	"github.com/saint-yellow/go-pl/toys/goblog/config"
 	"github.com/saint-yellow/go-pl/toys/goblog/pkg/logger"
 )
 
-func main() {
+func init() {
+	// 初始化配置信息
+	config.Initialize()
+}
 
+func main() {
+	// 初始化 SQL
     bootstrap.SetupDB()
+
+	// 初始化路由绑定
     router := bootstrap.SetupRoute()
 
     err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
--- toys/goblog/pkg/model/model.go
+++ toys/goblog/pkg/model/model.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"fmt"
+
+	"github.com/saint-yellow/go-pl/toys/goblog/pkg/config"
 	"github.com/saint-yellow/go-pl/toys/goblog/pkg/logger"
 	"gorm.io/gorm"
 
@@ -18,13 +21,29 @@ func ConnectDB() *gorm.DB {
 
     var err error
 
+    // 初始化 MySQL 连接信息
     config := mysql.New(mysql.Config{
-        DSN: "root:saint-yellow@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+        DSN: fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=%v&parseTime=True&loc=Local",
+            config.GetString("database.mysql.username"),
+            config.GetString("database.mysql.password"),
+            config.GetString("database.mysql.host"),
+            config.GetString("database.mysql.port"),
+            config.GetString("database.mysql.database"),
+            config.GetString("database.mysql.charset")),
     })
+
+    var level gormlogger.LogLevel
+    if config.GetBool("app.debug") {
+        // 读取不到数据也会显示
+        level = gormlogger.Warn
+    } else {
+        // 只有错误才会显示
+        level = gormlogger.Error
+    }
 
     // 准备数据库连接池
     DB, err = gorm.Open(config, &gorm.Config{
-        Logger: gormlogger.Default.LogMode(gormlogger.Warn),
+        Logger: gormlogger.Default.LogMode(level),
     })
 
     logger.LogError(err)
