9c8caabe9fb63dea6071f697aee4a90e4a0cfb9b
黄炯辉 <saint-yellow@hotmail.com>
2022-01-24 01:16:20 +0800

    手机是否注册接口

--- toys/gohub/bootstrap/database.go
+++ toys/gohub/bootstrap/database.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/saint-yellow/go-pl/toys/gohub/app/models/user"
 	"github.com/saint-yellow/go-pl/toys/gohub/pkg/config"
 	"github.com/saint-yellow/go-pl/toys/gohub/pkg/database"
 	"gorm.io/driver/mysql"
@@ -48,4 +49,7 @@ func SetupDatabase() {
     database.SQLDB.SetMaxIdleConns(config.GetInt("database.mysql.max_idle_connections"))
     // 设置每个链接的过期时间
     database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
+
+    // 自动迁移
+    database.DB.AutoMigrate(&user.User{})
 }
--- toys/gohub/routes/api.go
+++ toys/gohub/routes/api.go
@@ -2,9 +2,8 @@
 package routes
 
 import (
-    "net/http"
-
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/saint-yellow/go-pl/toys/gohub/app/http/controllers/api/v1/auth"
 )
 
 // RegisterAPIRoutes 注册网页相关路由
@@ -13,12 +12,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
     // 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
     v1 := r.Group("/v1")
     {
-        // 注册一个路由
-        v1.GET("/", func(c *gin.Context) {
-            // 以 JSON 格式响应
-            c.JSON(http.StatusOK, gin.H{
-                "Hello": "World!",
-            })
-        })
+        authGroup := v1.Group("/auth")
+        {
+            suc := new(auth.SignupController)
+            // 判断手机是否注册
+            authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+        }
     }
 }
