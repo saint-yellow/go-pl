d53f061542dbc8935a4fdce0ba61f72a804d49a5
黄炯辉 <saint-yellow@hotmail.com>
2022-01-24 05:19:04 +0800

    Email + 邮件验证码注册

--- toys/gohub/routes/api.go
+++ toys/gohub/routes/api.go
@@ -22,6 +22,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
             authGroup.POST("/signup/email/exist", suc.IsEmailExist)
             // 使用手机注册
             authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
+            // 使用邮箱注册
+            authGroup.POST("/signup/using-email", suc.SignupUsingEmail)
 
             // 验证码相关的接口
             vcc := new(auth.VerifyCodeController)
