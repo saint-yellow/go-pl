4ed02768cdaad368c56952981483b5f729c0cf72
黄炯辉 <saint-yellow@hotmail.com>
2022-01-23 23:17:10 +0800

    不使用默认的端口号和链接

--- toys/goblog/config/app.go
+++ toys/goblog/config/app.go
@@ -15,12 +15,12 @@ func init() {
         "debug": config.Env("APP_DEBUG", false),
 
         // 应用服务端口
-        "port": config.Env("APP_PORT", "3000"),
+        "port": config.Env("APP_PORT"),
 
         // gorilla/sessions 在 Cookie 中加密数据时使用
         "key": config.Env("APP_KEY", "33446a9dcf9ea060a0a6532b166da32f304af0de"),
 
         // 用以生成链接
-        "url": config.Env("APP_URL", "http://localhost:3000"),
+        "url": config.Env("APP_URL"),
     })
 }
