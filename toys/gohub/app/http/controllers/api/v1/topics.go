e18cc46e57048af6cf5aaaf3bbb48efa99128209
黄炯辉 <saint-yellow@hotmail.com>
2022-01-24 21:42:42 +0800

    更新话题

--- toys/gohub/app/http/controllers/api/v1/topics.go
+++ toys/gohub/app/http/controllers/api/v1/topics.go
@@ -14,7 +14,6 @@ type TopicsController struct {
 }
 
 func (ctrl *TopicsController) Store(c *gin.Context) {
-
     request := requests.TopicRequest{}
     if ok := requests.Validate(c, &request, requests.TopicSave); !ok {
         return
@@ -31,5 +30,28 @@ func (ctrl *TopicsController) Store(c *gin.Context) {
         response.Created(c, topicModel)
     } else {
         response.Abort500(c, "创建失败，请稍后尝试~")
+    }
+}
+
+func (ctrl *TopicsController) Update(c *gin.Context) {
+    topicModel := topic.Get(c.Param("id"))
+    if topicModel.ID == 0 {
+        response.Abort404(c)
+        return
+    }
+
+    request := requests.TopicRequest{}
+    if ok := requests.Validate(c, &request, requests.TopicSave); !ok {
+        return
+    }
+
+    topicModel.Title = request.Title
+    topicModel.Body = request.Body
+    topicModel.CategoryID = request.CategoryID
+    rowsAffected := topicModel.Save()
+    if rowsAffected > 0 {
+        response.Data(c, topicModel)
+    } else {
+        response.Abort500(c, "更新失败，请稍后尝试~")
     }
 }
